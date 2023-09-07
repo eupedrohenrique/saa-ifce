db7709f7f37bf1591484e4402f5e30719d8a2027
Pedro Henrique <eupedrohenrique.euzebio@gmail.com>
2023-09-07 13:28:19 -0300

    :sparkles: feat: add component Armario, Home, CenteredContainer

---
+++ src/components/Armario.js
@@ -0,0 +1,27 @@
+import React from "react"
+import styled from "styled-components"
+import { FaLock, FaUnlockAlt } from "react-icons/fa"
+
+const ArmarioContainer = styled.div`
+  border: 2px solid #ccc;
+  padding: 10px;
+  margin: 10px;
+  background-color: ${(props) => (props.disponivel ? "#7FFF7F" : "#FF7F7F")};
+  max-width: 500px;
+
+  @media (max-width: 500px) {
+    width: 50%; /* Em telas menores, ocupa 100% da largura */
+  }
+`
+
+const Armario = ({ numero, disponivel }) => {
+  return (
+    <ArmarioContainer disponivel={disponivel}>
+      {disponivel ? <FaUnlockAlt size={32} /> : <FaLock size={32} />}
+      <p>Número do Armário: {numero}</p>
+      <p>Disponível: {disponivel ? "Sim" : "Não"}</p>
+    </ArmarioContainer>
+  )
+}
+
+export default Armario
---
+++ src/components/Home.js
@@ -0,0 +1,32 @@
+import React from "react"
+import Armario from "./Armario"
+import CenteredContainer from "./CenteredContainer"
+
+const Home = () => {
+  // Simula dados de armários
+  const armarios = [
+    { numero: 1, disponivel: true },
+    { numero: 2, disponivel: false },
+    { numero: 3, disponivel: true },
+    { numero: 4, disponivel: false },
+  ]
+
+  return (
+    <CenteredContainer>
+      <h1>Lista de Ármarios</h1>
+      <div
+        style={{ display: "flex", flexWrap: "wrap", justifyContent: "center" }}
+      >
+        {armarios.map((armario) => (
+          <Armario
+            key={armario.numero}
+            numero={armario.numero}
+            disponivel={armario.disponivel}
+          />
+        ))}
+      </div>
+    </CenteredContainer>
+  )
+}
+
+export default Home
